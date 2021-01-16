bc65d0054fc528a7504ba92230ed5d8d1d3a4855
Fay Vera <fvera.cavalcante@gmail.com>
2021-01-15 23:36:45 -0500

    commit tree bf3402fb4dac9cf81a4c7a9a77161b179c0510f0

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+require 'pry'
 
+def sort_array_asc(integers)
+    integers.sort 
+end
+
+
+def sort_array_desc(array)
+    array.sort do |a,b|
+        b <=> a
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort do |x, y|
+        x.length <=> y.length
+    end
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    return array
+end
+
+def reverse_array(array)
+    array.sort {|a, b| return array.reverse}
+end
+
+def kesha_maker(arr)
+    arr.collect do |x|
+       x[2] = "$"
+    end
+    arr
+end
+
+def find_a(array)
+    array.select do |item|
+        item.start_with?("a")  
+    end
+end
+
+def sum_array(arr)
+    arr.inject(0) {|x, y| x + y}
+end
+
+def add_s(array)
+    array.each_with_index.collect do |element, index| 
+        if index != 1
+             "#{element}s"
+        else    
+             "#{element}"
+        end
+    end
+end
